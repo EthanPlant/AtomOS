434f465b915e5043f4f872435cc03c7b0bfe3822
Ethan Plant <plant.ethan@gmail.com>
2021-05-15 19:02:22 -0400

    Write some text using BIOS routines

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,9 +1,20 @@
-; The most basic boot sector possible
+; A simple boot sector that prints "Hello" the the screen with BIOS interrupts
 
-loop:
-    jmp loop ; Set up an infinite loop
+mov ah, 0x0e ; Set to tele-type mode
 
-; Pad with 0s
+; Write "Hello"
+mov al, 'H' ; Write H to al
+int 0x10 ; Call the screen interrupt
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10
+mov al, 'o'
+int 0x10
+
+jmp $ ; infinite loop
+
+; Padding and magic bootsect number
 times 510-($-$$) db 0
-
-dw 0xaa55 ; Magic number
+dw 0xaa55
