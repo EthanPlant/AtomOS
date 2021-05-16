03f95bde2066793809d5dbaf81bddca543b0de8c
Ethan Plant <plant.ethan@gmail.com>
2021-05-15 20:30:25 -0400

    Add disk reading

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,26 +1,41 @@
-; A simple boot sector that prints a string
+; A simple boot sector 
 
 [org 0x7c00] ; Offset our code
 
-; Print our strings
-mov bx, HELLO_MSG
-call print_string
+mov [BOOT_DRIVE], dl ; Store the boot drive
+
+; Initialize the stack
+mov bp, 0x8000
+mov sp, bp
+
+; Load 5 sectors from the disk
+mov bx, 0x9000
+mov dh, 5
+mov dl, [BOOT_DRIVE]
+call disk_load
+
+mov dx, [0x9000] ; Print the first loaded word
+call print_hex
 
 call print_nl
 
-mov bx, GOODBYE_MSG
-call print_string
+mov dx, [0x9000 + 512] ; Print the first word from the second loaded sector
+call print_hex
 
 jmp $ ; Hang
 
 %include "print_string.asm"
+%include "print_hex.asm"
+%include "disk_load.asm"
 
 ; Data
-HELLO_MSG:
-    db 'Hello, World',0
-GOODBYE_MSG:
-    db 'Goodbye',0
+BOOT_DRIVE:
+    db 0
 
 ; Padding and magic number
 times 510-($-$$) db 0
 dw 0xaa55
+
+; Add some dummy data beyond the boot sector
+times 256 dw 0xdada
+times 256 dw 0xface
